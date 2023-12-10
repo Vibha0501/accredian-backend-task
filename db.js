0656e339f9e87a0b7f8d710863610442b2079daf
vibha <alurvibha@gmail.com>
2023-12-10 23:41:56 +0530

    fixed errors

--- db.js
+++ db.js
@@ -1,20 +1,16 @@
 
 const mysql = require("mysql2");
-async function connectDB() {
-  try {
-    const connection = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "Vibha@2001",
-      database: "accredian",
-      port: 3306, 
-    });
-    console.log("Connected to the database!");
-    return connection;
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    throw error;
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Vibha@2001",
+  database: "accredian",
+});
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
   }
-}
-
-module.exports = connectDB;
+  console.log("Connected to MySQL database");
+});
+module.exports = connection;
